53f22b65700c5760581b873d00d524c43328d20e
Vishnu c <vishnumalbro124@gmail.com>
2025-02-09 19:15:28 +0530

    google testing 4

--- Utils/User/googleAuth.js
+++ Utils/User/googleAuth.js
@@ -2,57 +2,48 @@ import passport from "passport";
 import passportGoogle from "passport-google-oauth20";
 import User from "../../Models/User/UserDetailsModel.js";
 import dotenv from "dotenv";
+dotenv.config(); 
 
-dotenv.config();
 
-const GoogleStrategy = passportGoogle.Strategy;
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENTID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://velomax.vishnuc.site/google/authentication",
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      try {
-        let existingUser = await User.findOne({ email: profile.emails[0].value });
-
-        if (existingUser) {
-          if (!existingUser.googleId) {
-            existingUser.googleId = profile.id;
-            await existingUser.save();
-          }
-          return done(null, existingUser);
-        }
-
-        const newUser = new User({
-          firstname: profile.name.givenName,
-          lastname: profile.name.familyName,
-          email: profile.emails[0].value,
-          googleId: profile.id,
-          isActive: true,
-          isBlock: false,
-          isVerified: true,
-        });
-
-        await newUser.save();
-        return done(null, newUser);
-      } catch (error) {
-        return done(error, null);
+passport.use(new passportGoogle.Strategy(
+  {
+    clientID: process.env.GOOGLE_CLIENTID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "https://velomax.vishnuc.site/google/authentication",
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      let existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
       }
+
+      const newUser = new User({
+        firstname: profile.name.givenName,
+        lastname: profile.name.familyName,
+        email: profile.emails[0].value,
+        googleId: profile.id,
+        isActive: true,
+        isBlock: false,
+        isVerified: true,
+      });
+
+      await newUser.save();
+      return done(null, newUser);
+    } catch (error) {
+      return done(error, null);
     }
-  )
-);
+  }
+));
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+  done(null, { id: user.id, email: user.email, isBlock: user.isBlock });
 });
 
-passport.deserializeUser(async (id, done) => {
+passport.deserializeUser(async (data, done) => {
   try {
-    const foundUser = await User.findById(id);
-    if (foundUser && !foundUser.isBlock) {
+    const foundUser = await User.findById(data.id);
+    if (foundUser && foundUser.isBlock === false) {
       done(null, foundUser);
     } else {
       done(new Error("User is blocked"), null);
